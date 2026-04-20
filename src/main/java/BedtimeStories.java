bc71b0d14fa1b33d7c9c108e6cc447e6a0d150dc
Nathan Wondim <nathanwondim16@gmail.com>
2026-04-20 15:30:38 -0700

    Completed Exercise

--- src/main/java/BedtimeStories.java
+++ src/main/java/BedtimeStories.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class BedtimeStories {
+    static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What story/file do you want to read? ");
+        String story = scanner.nextLine().strip().toLowerCase() + ".txt";
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(story));
+            String line;
+            int lineNumber = 1;
+            while((line = reader.readLine()) != null) {
+                System.out.println(lineNumber + ".\t" + line);
+                lineNumber++;
+            }
+        } catch (IOException e) {
+            System.out.println("Couldn't find file or something unexpected what wrong");
+        }
+    }
 }
